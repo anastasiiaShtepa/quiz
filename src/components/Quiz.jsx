098c65c2e108a2974cbfcd87002dfafcf845c134
anastasiia <a.shtepa@zazmic.com>
2024-02-20 18:31:09 +0100

    added interval and timeout

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import QUESTIONS from "../questions";
+import quiz from "../assets/quiz-complete.png"
+import QuestionTimer from "./QuestionTimer";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+ 
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
+  if (quizIsComplete){
+    return <div id="summary"><img src={quiz} alt="img" /><h2>Quiz Completed!</h2></div>
+  }
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort((a, b) => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div id="question">
+      <QuestionTimer timeout={10000} onTimeout={()=>handleSelectAnswer(null)} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
