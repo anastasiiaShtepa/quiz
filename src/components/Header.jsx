0d70b1f62b828e0c147b50e0d1ef791babed35a9
anastasiia <a.shtepa@zazmic.com>
2024-02-20 16:14:17 +0100

    added quiz answers

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logoImg  from "../assets/quiz-logo.png"
+const Header = () => {
+  return (
+    <header><img src={logoImg} alt='logo' /><h1>ReactQuiz</h1></header>
+  )
+}
+
+export default Header
